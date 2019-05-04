014c2d1a3d9f1bd910b6162d29b40220c50f5043
Chathurindu <100wpck@gmail.com>
2019-05-04 13:46:41 +0530

    Updated inventory add_book.java

--- app/src/main/java/com/example/ashimi/androidlibrary/Inventory_add_book.java
+++ app/src/main/java/com/example/ashimi/androidlibrary/Inventory_add_book.java
@@ -1,6 +1,7 @@
 package com.example.ashimi.androidlibrary;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -8,14 +9,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class Inventory_add_book extends AppCompatActivity {
     EditText book_id,book_name,category,location;
     Button add_book_button;
     DatabaseReference ref;
+    long maxID=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +33,18 @@ public class Inventory_add_book extends AppCompatActivity {
         add_book_button  = findViewById(R.id.add_book_button);
 
         ref = FirebaseDatabase.getInstance().getReference().child("Book");
+        ref.addValueEventListener( new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                    maxID = (dataSnapshot.getChildrenCount());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        } );
 
        add_book_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,7 +71,7 @@ public class Inventory_add_book extends AppCompatActivity {
                         book.getCategory();
                         book.getLocation();
 
-                        ref.push().setValue(book);
+                        ref.child( String.valueOf( maxID+1 ) ).setValue(book);
 
                         Toast.makeText(Inventory_add_book.this, "Data Inserted Successfully", Toast.LENGTH_LONG).show();
 
