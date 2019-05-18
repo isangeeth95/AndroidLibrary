984f894847b051122bd666d29f03439d37745786
isangeeth95 <36575796+isangeeth95@users.noreply.github.com>
2019-05-19 01:42:27 +0530

    profile update

--- app/src/main/java/com/example/ashimi/androidlibrary/s_profile.java
+++ app/src/main/java/com/example/ashimi/androidlibrary/s_profile.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -46,6 +47,25 @@ public class s_profile extends AppCompatActivity {
                 Toast.makeText(s_profile.this, "Sorry..... something is wrong.", Toast.LENGTH_SHORT).show();
             }
         });
+
+        get_profileForm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                System.out.println(dbName.getText().toString().trim());
+                System.out.println(dbMobile.getText().toString().trim());
+
+                String user_name = dbName.getText().toString().trim();
+                String mobile_number = dbMobile.getText().toString().trim();
+
+                if(validate(user_name, mobile_number)){
+                    mData.child(user.getUid()).child("user_name").setValue(user_name);
+                    mData.child(user.getUid()).child("mobile_number").setValue(mobile_number);
+                }
+            }
+        });
     }
 
     private void setupUIViews(){
@@ -54,5 +74,22 @@ public class s_profile extends AppCompatActivity {
         dbEmail = (TextView) findViewById(R.id.dbEmail);
         dbMobile = (TextView)findViewById(R.id.dbMobile);
         get_profileForm = (CardView)findViewById(R.id.card_getProfileForm);
+    }
+
+    private Boolean validate(String user, String mobile){
+        Boolean result = false;
+
+        if(user == null || mobile == null){
+            Toast.makeText(s_profile.this, "Dont keep blank lines", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(!android.util.Patterns.PHONE.matcher(mobile).matches()){
+            Toast.makeText(s_profile.this, "Enter a valid mobile number", Toast.LENGTH_SHORT).show();
+        }
+
+        else{
+            result = true;
+        }
+        return result;
     }
 }
