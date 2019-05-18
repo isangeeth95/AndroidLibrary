a66c5673bb5c64dc78dadd4da8e19cb4e096b3ca
isangeeth95 <36575796+isangeeth95@users.noreply.github.com>
2019-05-18 15:19:54 +0530

    update design in register activity

--- app/src/main/java/com/example/ashimi/androidlibrary/s_register.java
+++ app/src/main/java/com/example/ashimi/androidlibrary/s_register.java
@@ -15,11 +15,10 @@ import android.widget.Toast;
 
 public class s_register extends AppCompatActivity {
 
-    DataBaseHelper db;
-    EditText fName_Register;
-    EditText lName_Register;
+    EditText name_Register;
     EditText address_Register;
     EditText email_Register;
+    EditText tp_Register;
     EditText password_Register;
     EditText confirmPassword_Register;
     Button register_Btn;
@@ -31,11 +30,8 @@ public class s_register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s_register);
 
-        db = new DataBaseHelper(this);
-        fName_Register = (EditText) findViewById(R.id.firstName_Register);
-        lName_Register = (EditText) findViewById(R.id.lastName_Register);
-        address_Register = (EditText) findViewById(R.id.address_Register);
-        //tp_Register = findViewById(R.id.telephone_Register);
+        name_Register = (EditText) findViewById(R.id.Name_Register);
+        tp_Register = findViewById(R.id.telephone_Register);
         email_Register = (EditText) findViewById(R.id.email_Register);
         password_Register = (EditText) findViewById(R.id.password);
         confirmPassword_Register = (EditText) findViewById(R.id.confirmPassword_Register);
@@ -46,29 +42,27 @@ public class s_register extends AppCompatActivity {
         register_Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String f_name = fName_Register.getText().toString().trim();
-                String l_name = lName_Register.getText().toString().trim();
+                String f_name = name_Register.getText().toString().trim();
                 String address = address_Register.getText().toString().trim();
                 String email = email_Register.getText().toString().trim();
                 String password = password_Register.getText().toString().trim();
                 String conf_password = confirmPassword_Register.getText().toString().trim();
 
-                if(password.equals(conf_password)){
-                    long val = db.addUser(f_name, l_name, address, email, password);
-
-                    if(val > 0){
-                        Toast.makeText(s_register.this, "Successfully registered as "+email+" .", Toast.LENGTH_SHORT).show();
-
-                        Intent moveToLogin = new Intent(s_register.this, s_login.class);
-                        startActivity(moveToLogin);
-                    }
-                    else{
-                        Toast.makeText(s_register.this, "Registration failed"+email+" .", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                else{
-                    Toast.makeText(s_register.this, "Password is not matching"+email+" .", Toast.LENGTH_SHORT).show();
-                }
+//                if(password.equals(conf_password)){
+//
+//                    if(){
+//                        Toast.makeText(s_register.this, "Successfully registered as "+email+" .", Toast.LENGTH_SHORT).show();
+//
+//                        Intent moveToLogin = new Intent(s_register.this, s_login.class);
+//                        startActivity(moveToLogin);
+//                    }
+//                    else{
+//                        Toast.makeText(s_register.this, "Registration failed"+email+" .", Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//                else{
+//                    Toast.makeText(s_register.this, "Password is not matching"+email+" .", Toast.LENGTH_SHORT).show();
+//                }
             }
         });
 
