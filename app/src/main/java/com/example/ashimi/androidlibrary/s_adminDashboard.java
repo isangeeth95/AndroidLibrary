f23c8f258ee4761bd08e94bc7848866eee5941c8
Chathurindu <100wpck@gmail.com>
2019-05-20 12:27:26 +0530

    add_admin pannel interface

--- app/src/main/java/com/example/ashimi/androidlibrary/s_adminDashboard.java
+++ app/src/main/java/com/example/ashimi/androidlibrary/s_adminDashboard.java
@@ -1,13 +1,40 @@
 package com.example.ashimi.androidlibrary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
 
 public class s_adminDashboard extends AppCompatActivity {
+
+    LinearLayout bookManage;
+    LinearLayout userManage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_s_admin_dashboard);
+        setContentView(R.layout.admin_panel);
+
+        bookManage = (LinearLayout) findViewById(R.id.bookManage);
+        userManage = (LinearLayout) findViewById(R.id.userManage);
+
+        bookManage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent_view_book=new Intent(s_adminDashboard.this, Inventory_main.class);
+                startActivity(intent_view_book);
+            }
+        });
+//        userManage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+////                Intent intent_add_book=new Intent(s_adminDashboard.this, AddBook.class);
+////                startActivity(intent_add_book);
+//            }
+//        });
     }
 }
